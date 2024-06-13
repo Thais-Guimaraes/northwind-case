0e4d598f4c7fe245c42d9debedf1d0266df96143
Thais Guimaraes <thaisbeguima@gmail.com>
2024-06-13 18:06:18 +0000

    criada dimensão produto

--- models/staging/erp/stg_erp__fornecedores.sql
+++ models/staging/erp/stg_erp__fornecedores.sql
@@ -4,7 +4,7 @@ with
             cast(C1 as int) as pk_fornecedor
             , cast(C2 as varchar) as nm_fornecedor
             , cast(C6 as varchar) as cidade_fornecedor
-            , cast(C9 as varchar) as pais_forncedor
+            , cast(C9 as varchar) as pais_fornecedor
         from {{ source('erp', 'supplier') }}
         where C1 != 'Id'
     )
